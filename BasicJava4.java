63998ce1917f7a6fa47b9152335c61279f785f8c
berlianristy <91619500+berlianristy@users.noreply.github.com>
2021-10-18 22:26:08 +0700

    Java-Challenge-4
    Java Methods

---
+++ BasicJava4.java
@@ -0,0 +1,16 @@
+public class BasicJava4 {
+    public static void main(String[] args) { 
+       umur(20);
+    }  
+    static void umur(int umur) {
+  
+      if (umur >= 17) {
+        System.out.println("Anda sudah remaja"); 
+        
+      } else {
+        System.out.println("Anda belum remaja"); 
+      }
+      
+    } 
+}
+    
